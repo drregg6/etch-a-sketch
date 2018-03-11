0b4870e34d3d3ffd5f138da82bf7a3319716e013
Dave <drregg6@gmail.com>
2018-03-11 13:49:10 -0400

    added general js skeleton

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,21 @@
+// .container's width should be 960px
 const container = document.querySelector('.container');
 
+// window.onload = createGrid();
+/* 
+function createGrid(width=16) {
+    for (let i = 0; i <= width; i++) {
+        if (width % 16 === 0) {
+            const row = document.createElement('div');
+            div.classList.add('row');
+            div.appendChild('.container');
+        }
+        const div = document.createElement('div');
+        div.classList.add('default-content');
+        div.appendChild('.row');
+    }
+}
+*/
 // create a grid - 16x16 (256 total spaces)
 // for each number up to 256
 //      create a new column element for every 16 divs
@@ -10,3 +26,7 @@ const container = document.querySelector('.container');
 //          create a new
 
 // suggest using a flexbox for container (set width)
+// these will be SMALL divs
+
+// div.addeventlisten(hover / mouseenter, mouseleave)
+// add black
