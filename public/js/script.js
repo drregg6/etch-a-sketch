4f4d1add648fab7b474ef43a466422c7c4d7759a
Dave <drregg6@gmail.com>
2018-03-11 13:35:17 -0400

    added basic content to html / css, added comments to js

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+
+// create a grid - 16x16 (256 total spaces)
+// for each number up to 256
+//      create a new column element for every 16 divs
+//      create a new div element
+//      add a class to that div
+//      append it to the current column
+//      if the number % 16 === 0
+//          create a new
+
+// suggest using a flexbox for container (set width)
